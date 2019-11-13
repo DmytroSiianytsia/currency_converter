3ffeac2b97f8ac2f37fe1d9f4543c1acd78b9c2d
Dima <ds081181@gmail.com>
2019-11-14 01:02:30 +0200

    Updates

---
+++ precache-manifest.1bee46f1ecba5efd31c453d94d58bb0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcfc144941a5e4566657d376c95e2e9",
+    "url": "/currency_converter/index.html"
+  },
+  {
+    "revision": "76de4390dd3f5ca520ba",
+    "url": "/currency_converter/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7d86dba418a84bbb23d4",
+    "url": "/currency_converter/static/js/2.e61af77f.chunk.js"
+  },
+  {
+    "revision": "76de4390dd3f5ca520ba",
+    "url": "/currency_converter/static/js/main.33c7c45c.chunk.js"
+  },
+  {
+    "revision": "91b911bf200717cf4d82",
+    "url": "/currency_converter/static/js/runtime-main.0ec8a3e0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/currency_converter/static/media/logo.25bf045c.svg"
+  }
+]);
